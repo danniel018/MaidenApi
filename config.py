3b7b3419e9ab176ec106895bc0ac0694a9147274
Daniel Pérez <daniel_p0818@hotmail.com>
2022-09-17 14:44:22 -0500

    ignore config

--- config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Maiden19@localhost/maidentest1'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SECRET_KEY = 'Amon_Amarth-@tHe098*GreaT_heathen&%$armY-megadeTH_<()&/the23sicCK.TheDying-_-_and{the++*DEAD!!0920'
-    JWT_ERROR_MESSAGE_KEY = 'message'
